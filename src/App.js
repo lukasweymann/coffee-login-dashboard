287058ad4830b76a64194b47874932f36020e300
Lukas <lukas_weymann@hotmail.com>
2020-07-07 17:32:42 +0200

    Added initial dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+const [username, setUsername] = useState(null);
+const [password, setPassword] = useState(null);
+
+function handleUsername(event){
+  setUsername(event.target.value);
+}
+function handlePassword(event){
+  setPassword(event.target.value);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img className="login-logo" src={require("../src/images/welovecaffeine-logo.png")} alt="company's logo"/>
+     <form >
+     <input onChange={handleUsername} type="text" placeholder="Username" name="username"/>
+ 
+     <input onChange={handlePassword} type="password" placeholder="Password" name="password"/>
+     <button  type="submit">LOG IN</button>
+     </form>
     </div>
+
   );
-}
+} 
 
 export default App;
